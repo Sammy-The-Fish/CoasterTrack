de40107f857ca370bcfef84f6b536e0e4ade0c5a
sam <sln581@student.bham.ac.uk>
2026-06-26 21:12:31 +0100

    improved new details screen

--- app/src/main/java/com/example/coastertrack/ui/screens/NewDetailsScreen.kt
+++ app/src/main/java/com/example/coastertrack/ui/screens/NewDetailsScreen.kt
@@ -2,13 +2,16 @@ package com.example.coastertrack.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +24,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
@@ -28,15 +33,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material3.Text
 import com.example.coastertrack.R
+import com.example.coastertrack.ui.components.RollercoasterDetails
+import com.example.coastertrack.ui.screens.MainStatistic
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -58,7 +68,13 @@ fun NewDetailsScreen(
 
 
     val testImage = painterResource(R.drawable.stealth_up_close_2024_05_08_02)
-    Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.surface))
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    )
 
     Box(modifier = Modifier.padding()) {
 
@@ -73,39 +89,124 @@ fun NewDetailsScreen(
                 contentScale = ContentScale.Crop,
                 contentDescription = ""
             )
-            Box(
-                modifier = Modifier
-                    .offset(x = 0.dp, y = (-40).dp)
-                    .clip(RoundedCornerShape(40.dp))
-                    .background(color = MaterialTheme.colorScheme.surface)
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-            ) {
-                Row {
-                    Text(
-                        "Hyperia",
-                        fontFamily = robotoSerif,
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = 48.sp,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        modifier = Modifier.padding(15.dp, 20.dp)
-                            .weight(2f)
-                    )
-                    Box (
-                        modifier = Modifier
-                            .weight(1f)
-                            .aspectRatio(1f)
-                            .padding(10.dp)
-                            .clip(queueTimeShape)
-                            .background(MaterialTheme.colorScheme.primaryContainer)
-                    ){
+            Box {
+                Box(
+                    modifier = Modifier
+                        .offset(x = 0.dp, y = (-40).dp)
+                        .clip(RoundedCornerShape(40.dp))
+                        .background(color = MaterialTheme.colorScheme.surface)
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                )
+                Column (
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth().offset(0.dp, (-40).dp)
+                ) {
+                    Row(modifier = Modifier) {
+                        Text(
+                            "Hyperia",
+                            fontFamily = robotoSerif,
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize = 48.sp,
+                            color = MaterialTheme.colorScheme.onSecondaryContainer,
+                            modifier = Modifier
+                                .padding(15.dp, 10.dp)
+                            //                            .weight(2f)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(150.dp)
+                                .aspectRatio (1f)
+                                .offset(0.dp, (-75).dp)
+                                .padding(10.dp)
+                                .clip(queueTimeShape)
+                                .background(MaterialTheme.colorScheme.primaryContainer)
 
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                androidx.compose.material3.Text(
+                                    buildAnnotatedString {
+                                        append("40" ?: "---")
+                                        withStyle(style = SpanStyle(fontSize = 25.sp)) {
+                                            append("min")
+                                        }
+                                    },
+                                    style = MaterialTheme.typography.displayMediumEmphasized,
+                                    fontWeight = FontWeight.Medium,
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+                        }
+                    }
+                    Column(modifier = Modifier.offset(0.dp, (-75).dp)) {
+                        RollercoasterDetails(
+                            statistic1 = {
+                                MainStatistic(
+                                    name = "Height:",
+                                    value = "65",
+                                    "m"
+                                )
+                            },
+                            statistic2 = {
+                                MainStatistic(
+                                    name = "Speed:",
+                                    value = "6",
+                                    "km/h"
+                                )
+                            },
+                            statistic3 = {
+                                MainStatistic(
+                                    name = "Length:",
+                                    value = "56",
+                                    "m"
+                                )
+                            },
+                            statistic4 = {
+                                MainStatistic(
+                                    name = "Inversions",
+                                    value = "20",
+                                    ""
+                                )
+                            },
+                        )
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(25.dp, 0.dp)
+                                .clip(RoundedCornerShape(25.dp))
+                                .background(color = MaterialTheme.colorScheme.surfaceContainer)
+                                .padding(15.dp)
+                        ) {
+                            Spacer(Modifier.height(200.dp))
+//                state.details.statistics.forEach { item ->
+//                    Row(
+//                        horizontalArrangement = Arrangement.Start
+//                    ) {
+//                        androidx.compose.material3.Text(
+//                            text = item.name + ": ",
+//                            fontWeight = FontWeight.Bold
+//                        )
+//                        androidx.compose.material3.Text(
+//                            text = item.value
+//                        )
+//                    }
+//                }
+                        }
+                        Spacer(modifier = Modifier.height(100.dp))
                     }
                 }
-
             }
 
+
         }
+
+
         IconButton(
             modifier = Modifier
                 .align(Alignment.TopStart)
